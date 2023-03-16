f6457c2257018f25e3d4598cb8d223e2cecc2e5f
Daejung <minerva01st@gmail.com>
2023-03-16 18:11:03 +0900

    Four_LED / 비효율적인 코드 반복문으로 수정

--- Four_LED/Four_LED/main.c
+++ Four_LED/Four_LED/main.c
@@ -16,24 +16,47 @@ int main(void)
 	PORTA = 0x00;	// 출력 상태 0으로 초기화
     while (1) 
     {
-		PORTA &= ~(0x01);
-		PORTA |= 0x01 << 1;
-		delay(1000);
-		PORTA &= ~(0x01 << 1);
-		PORTA |= 0x01 << 2;
-		delay(1000);
-		PORTA &= ~(0x01 << 2);
-		PORTA |= 0x01 << 3;
-		delay(1000);
-		PORTA &= ~(0x01 << 3);
-		PORTA |= 0x01 << 2;
-		delay(1000);
-		PORTA &= ~(0x01 << 2);
-		PORTA |= 0x01 << 1;
-		delay(1000);
-		PORTA &= ~(0x01 << 1);
-		PORTA |= 0x01;
-		delay(1000);
+		
+		for (int i = 0; i < 3; i++){
+			PORTA &= ~((0x01) << i);
+			PORTA |= (0x01) << (i + 1);
+			delay(1000);
+		}
+		for (int i = 3; i > 0; i--){
+			PORTA &= ~((0x01) << i);
+			PORTA |= (0x01) << (i - 1);
+			delay(1000);
+		}
+		
+		//// 0번 끄고 1번 켜기
+		//PORTA &= ~(0x01);
+		//PORTA |= 0x01 << 1;
+		//delay(1000);
+		//
+		//// 1번 끄고 2번 켜기
+		//PORTA &= ~(0x01 << 1);
+		//PORTA |= 0x01 << 2;
+		//delay(1000);
+		//
+		//// 2번 끄고 3번 켜기
+		//PORTA &= ~(0x01 << 2);
+		//PORTA |= 0x01 << 3;
+		//delay(1000);
+		//
+		//// 3번 끄고 2번 켜기
+		//PORTA &= ~(0x01 << 3);
+		//PORTA |= 0x01 << 2;
+		//delay(1000);
+		//
+		//// 2번 끄고 1번 켜기
+		//PORTA &= ~(0x01 << 2);
+		//PORTA |= 0x01 << 1;
+		//delay(1000);
+		//
+		//// 1번 끄고 0번 켜기
+		//PORTA &= ~(0x01 << 1);
+		//PORTA |= 0x01;
+		//delay(1000);
     }
 }
 
